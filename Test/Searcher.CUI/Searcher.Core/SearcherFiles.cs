8ce7dfc99dfe1e7c9e44cba60d3042e312f29a27
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-10-05 20:05:08 +0400

    add functionality that searches text files (* .txt) for the specified substring and displays a list of such files

---
+++ Test/Searcher.CUI/Searcher.Core/SearcherFiles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Searcher.Core
+{
+    public static class SearcherFiles
+    {
+        public static IEnumerable<string> FindFilesContainSubstring(string folderPath,string substring)
+        {
+            return (from file in Directory.EnumerateFiles(folderPath, "*.txt") 
+                let contents = File.ReadAllText(file) 
+                let sentencesWithSubstring = contents.ExctractSubstrings(substring) 
+                let fileName = Path.GetFileName(file) 
+                where !sentencesWithSubstring.IsNullOrEmpty() select fileName)
+                .ToList();
+        }
+    }
+}
