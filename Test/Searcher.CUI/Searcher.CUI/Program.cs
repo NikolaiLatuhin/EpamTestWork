0db016c102aaed272a2f20b9f10c94b39e45d920
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-10-05 20:03:58 +0400

    add extension method that returns items in a list of strings, searches for an occurrence in a string.

---
+++ Test/Searcher.CUI/Searcher.CUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Searcher.Core;
+
+namespace Searcher.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"C:\TestWork\EpamTestWork\Test\Searcher.CUI\Searcher.CUI\bin\Debug";
+
+            var collection = SearcherFiles.FindFilesContainSubstring(path, "island");
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Test/Searcher.CUI/Searcher.Core/MyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Searcher.Core
+{
+    public static class MyExtensions
+    {
+        public static IEnumerable<string> ExctractSubstrings(this string inputString, string subString)
+        {
+            var sentences = inputString.Split('.', '!' , ',' , '?');
+            var substringSentences = sentences.Where(s => s.Contains(subString));
+            return substringSentences;
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null) return true;
+            return !source.Any();
+        }
+    }
+}
