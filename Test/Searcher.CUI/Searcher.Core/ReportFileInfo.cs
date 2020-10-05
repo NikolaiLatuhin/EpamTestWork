d1df509bc971ad39112b15fcc34c4a302ea378c4
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-10-05 20:50:07 +0400

    implementation methods in SearcherFiles

--- Test/Searcher.CUI/Searcher.Core/ReportFileInfo.cs
+++ Test/Searcher.CUI/Searcher.Core/ReportFileInfo.cs
@@ -1,18 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Searcher.Core
 {
+
     public class ReportFileInfo
     {
         public string Name { get; set; }
 
         public string Location { get; set; }
 
-        public int Size { get; set; }
+        public long Size { get; set; }
 
         public DateTime EditDateTime { get; set; }
 
-        public string ContainingString { get; set; }
+        public IEnumerable<string> ContainingString { get; set; }
 
         public int NumberOccurrences { get; set; }
 
--- Test/Searcher.CUI/Searcher.Core/SearcherFiles.cs
+++ Test/Searcher.CUI/Searcher.Core/SearcherFiles.cs
@@ -6,14 +6,49 @@ namespace Searcher.Core
 {
     public static class SearcherFiles
     {
-        public static IEnumerable<string> FindFilesContainSubstring(string folderPath,string substring)
+        public static IEnumerable<string> FindFilesContainSubstring(string folderPath,string substring, out List<ReportFileInfo> reports)
         {
-            return (from file in Directory.EnumerateFiles(folderPath, "*.txt") 
-                let contents = File.ReadAllText(file) 
-                let sentencesWithSubstring = contents.ExctractSubstrings(substring) 
-                let fileName = Path.GetFileName(file) 
-                where !sentencesWithSubstring.IsNullOrEmpty() select fileName)
-                .ToList();
+            var listWithSubstring = new List<string>();
+            var listReportFileInfo = new List<ReportFileInfo>();
+
+
+            foreach (var file in Directory.EnumerateFiles(folderPath, "*.txt"))
+            {
+                var contents = File.ReadAllText(file);
+                var sentencesWithSubstring = contents.ExctractSubstrings(substring);
+                var fileName = Path.GetFileName(file);
+
+                if (sentencesWithSubstring.IsNullOrEmpty()) continue;
+                listWithSubstring.Add(fileName);
+                var location = Path.GetFullPath(fileName);
+                var size = new FileInfo(fileName).Length;
+                var editDateTime = File.GetLastWriteTime(fileName);
+                var numberOccurrences = sentencesWithSubstring.Count();
+
+                var report = new ReportFileInfo
+                {
+                    Name = fileName, Location = location, Size = size,
+                    EditDateTime = editDateTime, NumberOccurrences = numberOccurrences,
+                    ContainingString = sentencesWithSubstring
+                };
+
+                listReportFileInfo.Add(report);
+            }
+
+            var sortedReports = SortDescendingReports(listReportFileInfo);
+
+            reports = sortedReports.ToList();
+
+            return listWithSubstring;
+
+        }
+
+        private static IEnumerable<ReportFileInfo> SortDescendingReports(IEnumerable<ReportFileInfo> listReportFileInfo)
+        {
+            var sortedReports = from r in listReportFileInfo
+                orderby r.NumberOccurrences descending
+                select r;
+            return sortedReports;
         }
     }
 }
