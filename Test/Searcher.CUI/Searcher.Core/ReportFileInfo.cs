166d0f1606966ec1f87973d75085fbf072e58264
Nikolai Latuhin <nickolay.latuhin@gmail.com>
2020-10-05 20:05:37 +0400

    add a report class for found files to the program

---
+++ Test/Searcher.CUI/Searcher.Core/ReportFileInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Searcher.Core
+{
+    public class ReportFileInfo
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public int Size { get; set; }
+
+        public DateTime EditDateTime { get; set; }
+
+        public string ContainingString { get; set; }
+
+        public int NumberOccurrences { get; set; }
+
+    }
+}
